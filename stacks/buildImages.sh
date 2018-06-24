4c1e887257e5b594e30718e57dd939f2c2da0a92
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-24 12:44:51 +0200

    preapring to test 2.1.1

--- stacks/buildImages.sh
+++ stacks/buildImages.sh
@@ -3,8 +3,8 @@ if [[ $projectDir && -d $projectDir ]];then
     pushd $projectDir #$HOME/dockerstore/ci-stack
 
     #build php
-    sudo docker build --no-cache ./rest-client-node -t linBox:5000/restnode:v2.1.0
-    sudo docker build --no-cache ./rest-api-dotnet -t linBox:5000/restdotnet:v2.1.0
+    sudo docker build --no-cache ./rest-client-node -t linBox:5000/restnode:v2.1.1
+    sudo docker build --no-cache ./rest-api-dotnet -t linBox:5000/restdotnet:v2.1.1
 
     #enable this during testing only
 
@@ -13,7 +13,8 @@ if [[ $projectDir && -d $projectDir ]];then
     # sudo docker push linBox:5000/restnode:v2.0.3
 
     #push image to registry
-    #sudo docker push linBox:5000/restnode:v2.0.2
+    sudo docker push linBox:5000/restnode:v2.1.1
+    sudo docker push linBox:5000/restdotnet:v2.1.1
 
     popd 
 
