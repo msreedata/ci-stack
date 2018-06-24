0836570a37b16a9d0d1b8f6ae2f7936be9c932a1
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-24 13:45:59 +0200

    ..getting ready to push the variable version to repo

--- stacks/buildImages.sh
+++ stacks/buildImages.sh
@@ -2,24 +2,22 @@
 if [[ $projectDir && -d $projectDir ]];then
     pushd $projectDir #$HOME/dockerstore/ci-stack
 
-    rest_api_version=v2.2.4
-    rest_client_version=v2.2.4
-    #build php
-    # sudo docker build --no-cache ./rest-client-node -t linBox:5000/restnode:v2.1.1
-    # sudo docker build --no-cache ./rest-api-dotnet -t linBox:5000/restdotnet:v2.1.1
+    # define versions
+    rest_api_version=v2.2.5
+    rest_client_version=v2.2.6
 
-    sudo docker build ./rest-client-node -t "linBox:5000/restnode:${rest_api_version}"
-    sudo docker build ./rest-api-dotnet -t linBox:5000/restdotnet:${rest_client_version}
-
+    #build images with tag
+    sudo docker build ./rest-api-dotnet -t linBox:5000/restdotnet:${rest_api_version}
+    sudo docker build ./rest-client-node -t "linBox:5000/restnode:${rest_client_version}"
 
     #enable this during testing only
-    # sudo docker build --no-cache ./rest-client-node -t restnode:v2.0.3
-    # sudo docker tag restnode:v2.0.3 linBox:5000/restnode:v2.0.3
-    # sudo docker push linBox:5000/restnode:v2.0.3
+    # sudo docker build --no-cache ./rest-client-node -t restnode:${rest_client_version}
+    # sudo docker tag restnode:${rest_client_version} linBox:5000/restnode:${rest_client_version}
+    # sudo docker push linBox:5000/restnode:${rest_client_version}
 
     #push image to registry
-    # sudo docker push linBox:5000/restnode:v2.1.1
-    # sudo docker push linBox:5000/restdotnet:v2.1.1
+    sudo docker push linBox:5000/restdotnet:${rest_api_version}
+    sudo docker push linBox:5000/restnode:${rest_client_version}
 
     popd 
 
