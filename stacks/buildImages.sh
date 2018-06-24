3339a5e5c035e3b64baf2994f7e5b7845aab34f9
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-24 13:39:59 +0200

    ..

--- stacks/buildImages.sh
+++ stacks/buildImages.sh
@@ -2,14 +2,15 @@
 if [[ $projectDir && -d $projectDir ]];then
     pushd $projectDir #$HOME/dockerstore/ci-stack
 
-    rest-api-version="v2.2.0"
-    rest-client-version="v2.2.1"
+    rest_api_version=v2.2.1
+    rest_client_version=v2.2.2
     #build php
     # sudo docker build --no-cache ./rest-client-node -t linBox:5000/restnode:v2.1.1
     # sudo docker build --no-cache ./rest-api-dotnet -t linBox:5000/restdotnet:v2.1.1
 
-    sudo docker build ./rest-client-node -t linBox:5000/restnode:${rest-api-version}
-    sudo docker build ./rest-api-dotnet -t linBox:5000/restdotnet:${rest-client-version}
+    sudo docker build ./rest-client-node -t "linBox:5000/restnode:${rest_api_version}"
+    sudo docker build ./rest-api-dotnet -t linBox:5000/restdotnet:${rest_client_version}
+
 
     #enable this during testing only
     # sudo docker build --no-cache ./rest-client-node -t restnode:v2.0.3
