13630fbb0dc6f56b8e48a87a76955d703dcb4c84
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-24 09:07:27 +0200

    ..

--- stacks/buildImages.sh
+++ stacks/buildImages.sh
@@ -3,7 +3,7 @@ if [[ $projectDir && -d $projectDir ]];then
     pushd $projectDir #$HOME/dockerstore/ci-stack
 
     #build php
-    sudo docker build --no-cache ./rest-client-node -t linBox:5000/restnode:v2.0.2
+    #sudo docker build --no-cache ./rest-client-node -t linBox:5000/restnode:v2.0.2
     #sudo docker build --no-cache ./rest-api-dotnet -t linBox:5000/restdotnet:v2
 
     #enable this during testing only
