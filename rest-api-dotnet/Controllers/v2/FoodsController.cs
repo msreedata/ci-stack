da3221544cc30934c485dc7a8fb5d592abcd5b63
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-24 23:20:23 +0200

    ..

--- rest-api-dotnet/Controllers/v2/FoodsController.cs
+++ rest-api-dotnet/Controllers/v2/FoodsController.cs
@@ -9,7 +9,7 @@ namespace SampleWebApiAspNetCore.v2.Controllers
         [HttpGet]
         public ActionResult Get()
         {
-            return Ok("2.3.2");
+            return Ok("2.3.3");
         }
     }
 }
