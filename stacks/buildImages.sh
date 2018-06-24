f8a429121e254f863c4d1573635d35807008a40b
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-24 23:06:50 +0200

    ..

--- stacks/buildImages.sh
+++ stacks/buildImages.sh
@@ -3,8 +3,8 @@ if [[ $projectDir && -d $projectDir ]];then
     pushd $projectDir #$HOME/dockerstore/ci-stack
 
     # define versions
-    rest_api_version=v2.3.2
-    rest_client_version=v2.3.2
+    rest_api_version=v2.3.3
+    rest_client_version=v2.3.3
 
     #build images with tag
     sudo docker build ./rest-api-dotnet -t linBox:5000/restdotnet:${rest_api_version}
